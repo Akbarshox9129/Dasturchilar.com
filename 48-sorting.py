389489ae477641438805fe7ef20595c0549192b5
Akbarshox <masterw9129@gmail.com>
2022-07-27 09:28:55 +0500

    'secondsaving'

--- 48-sorting.py
+++ 48-sorting.py
@@ -1,18 +1,9 @@
-# Sorting algorithms
 # BubbleSort
 def BubbleSort(num):
     for i in range(len(num)):
         for j in range(i+1,len(num)):
             if num[i]>num[j]:
                 num[i],num[j]=num[j],num[i]
-# SelectionSort
-def SelectionSort(num):
-    for i in range(len(num)):
-        min=i
-        for j in range(min+1,len(num)):
-            if num[j]<num[min]:
-                min=j
-        num[i],num[min]=num[min],num[i]
 # InsertionSort
 def InsertionSort(num):
     for i in range(1,len(num)):
@@ -22,6 +13,14 @@ def InsertionSort(num):
             num[j+1]=num[j]
             j-=1
         num[j+1]=a
+# SelectionSort
+def SelectionSort(num):
+    for i in range(len(num)):
+        min=i
+        for j in range(min+1,len(num)):
+            if num[j]<num[min]:
+                min=j
+        num[i],num[min]=num[min],num[i]
 sonlar=[7,6,4,9,2,1,8,3,4,7,9,3]
 #BubbleSort(sonlar)
 #SelectionSort(sonlar)
